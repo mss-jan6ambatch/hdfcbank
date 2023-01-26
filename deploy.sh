0494ed2afcf7c58e71d77948291eafc9b0829df4
jvrsatish <jvrsatish@gmail.com>
2023-01-26 19:31:04 +0530

    updated...

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 echo "This file is a deployment script file"
-echo "We've appended some stages of code logic in deploy.sh file" 
+echo "We've appended some stages of code logic in deploy.sh file"
+echo "Updated this file at 01/26 at 7:30 PM IST"
+
